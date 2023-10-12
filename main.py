a1d0562c1ffcb1c59bed3e008112a82f78b21c42
JasonBats <jason.bellut@gmail.com>
2023-10-12 19:33:31 +0200

    restructuration code / Reste à trier par catégories

--- main.py
+++ main.py
@@ -8,46 +8,38 @@ url = "http://books.toscrape.com/catalogue/page-1.html"
 reponse = requests.get(url)
 page = reponse.content
 
-# Parse du code HTML
+# Création du CSV + écriture
 
-page_number = 1
-
-# while reponse != 404: //// REMETTRE APRES TEST
-while page_number < 3:
-    url = "http://books.toscrape.com/catalogue/page-" + str(page_number) + ".html"
-    reponse = requests.get(url)
-    page = reponse.content
-    print(url)
-    print(reponse)
-    soup = BeautifulSoup(page, "html.parser")
-
-    articles = soup.find_all("article") # Cibler chaque article
+with open("datas.csv", "w", encoding="utf-8") as fichier_csv:
     en_tete = ["URL Produit", "Code UPC", "Titre", "Prix TTC", "Prix HT", "Stock Disponible", "Description", "Catégorie", "Note", "URL Image"]
-
-    with open("datas.csv", "w", encoding="utf-8") as fichier_csv:
-        writer = csv.writer(fichier_csv, delimiter=";")
-        writer.writerow(en_tete)
+    writer = csv.writer(fichier_csv, delimiter=";")
+    writer.writerow(en_tete)
+    page_number = 1
+    while reponse != 404: # Tant que l'URL ne renvoie pas de 404 : aller à la page suivante
+        url = "http://books.toscrape.com/catalogue/page-" + str(page_number) + ".html" # Ecriture de l'URL de page d'accueil / Actuellement 50 pages
+        print(url)
+        reponse = requests.get(url)
+        page = reponse.content
+        soup = BeautifulSoup(page, "html.parser")
+        articles = soup.find_all("article") # Cibler chaque article
         for article in articles:
-            h3_elements = article.find_all("h3") # Cibler chaque nom d'article
-
+            h3_elements = article.find_all("h3") # Dans chaque article, cibler chaque nom d'article
             for h3 in h3_elements:
-                a_elements = h3.find_all("a") # Cibler chaque url d'article
-
+                a_elements = h3.find_all("a") # Dans chaque nom d'article, récupérer le lien de l'article
                 for a in a_elements:
                     url_product = a["href"]
                     reponse_product = requests.get("http://books.toscrape.com/catalogue/" + url_product) # Concatener chaque url
                     page_product = reponse_product.content # Ouvrir chaque url
                     soup = BeautifulSoup(page_product, "html.parser") # Parser chaque page produit
-                    tables = soup.find_all("td")
-                    print(url_product)
-                    upc = tables[0]
-                    book_title = soup.find("h1") # Cibler le titre <h1> de la page
+                    tables = soup.find_all("td") # Cibler le tableau descriptif de l'article
+                    upc = tables[0] # Le code UPC correspond à la 1ère <td> du tableau
+                    book_title = soup.find("h1") # Cibler le titre <h1> de la page qui correspond au titre du livre
                     price_excl_tax = tables[2]
                     price_incl_tax = tables[3]
                     number_available = tables[5]
                     paragraphs = soup.find_all("p")
                     book_description = paragraphs[3] # Description = 4ème <p> de la page. Peut mieux faire ?
-                    ariane = soup.find("ul") # Chercher la liste d'ariane
+                    ariane = soup.find("ul") # Chercher la liste  présente dans le fil d'ariane
                     for items in ariane:
                         items = soup.find_all("li")
                         category = items[2] # Récupérer le 3e item de la liste dans le fil d'ariane > Correspond à la catégorie
@@ -78,9 +70,9 @@ while page_number < 3:
                             # print(category.text[1:][:-1]) # Slice les retours à la ligne
                             # print(note)
                             # print("http://books.toscrape.com" + image_url[5:])
-                    ligne = ["http://books.toscrape.com/" + url_product, upc.text, book_title.text, price_excl_tax.text[1:], price_incl_tax.text[1:], number_available.text[10:][:-10], book_description.text, category.text[1:][:-1], note, "http://books.toscrape.com" + image_url[5:]]
+                    ligne = ["http://books.toscrape.com/catalogue/" + url_product, upc.text, book_title.text, price_excl_tax.text[1:], price_incl_tax.text[1:], number_available.text[10:][:-10], book_description.text, category.text[1:][:-1], note, "http://books.toscrape.com" + image_url[5:]]
                     writer.writerow(ligne)
-    page_number += 1
+        page_number += 1
 
 
 print(reponse)
